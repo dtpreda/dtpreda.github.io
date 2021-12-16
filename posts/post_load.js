f5423a2bcfcbc8bff536e847d6bc2a5c7c5487d7
David Preda <dtpreda30@gmail.com>
2021-12-16 23:04:23 +0000

    Add split to page building

--- posts/post_load.js
+++ posts/post_load.js
@@ -5,27 +5,88 @@ async function text_load(path) {
         let text_data = await get_file(path);
 
         var text = text_data.split('\n');
+        var children = new Array();
 
-        
-        var div = document.createElement("div");
-        div.classList.add("content");
-        
         for (var i = 0; i < text.length / 2; i++) {
-            if (text[2*i] == "title") {
-                var h1 = document.createElement("h1");
+            if (text[2*i] == "--title") {
+                document.title = text[2*i + 1];
+            }
+            else if (text[2*i] == "--start intro") {
+                var intro = document.createElement("div");
+                intro.classList.add("content");
+
+                while (text[2*i] != "--end intro") {
+                    i++;
+                    add_content(text[2*i], text[2*i + 1], intro);
+                }
+
+                children.push(intro);
+            } else if (text[2*i] == "--start content") {
+                var content = document.createElement("div");
+                content.classList.add("content");
+
+                while (text[2*i] != "--end content") {
+                    i++;
+                    add_content(text[2*i], text[2*i + 1], content);
+                }
+
+                children.push(content);
+            } else if (text[2*i] == "--start split") {
+                var split =document.createElement("div");
+                split.classList.add("split");
                 
-                h1.innerHTML = text[2*i + 1];
-                div.appendChild(h1);
-            } else if (text[2*i] == "content") {
-                var p = document.createElement("p");
+                var n = 0;
+                while (text[2*i] != "--end split") {
+                    i++;
+                    
+                    if (text[2*i] == "--start") {
+                        n++;
+                        var div = document.createElement("div");
+                        div.classList.add("content");
+                        
+                        while (text[2*i] != "--end") {
+                            i++;
+                            add_content(text[2*i], text[2*i + 1], div);
+                        }
+                        split.appendChild(div);
+                    }
+                }
+
+                split.style.marginTop = "-10px";
                 
-                p.innerHTML = text[2*i +1];
-                div.appendChild(p);
+                var p = 100 / n;
+                for (var j = 0; j < split.children.length; j++) {
+                    var child = split.children[j];
+                    child.style.width = p.toString() + "%";
+                    if (j > 0) {
+                        child.style.marginLeft = "0";
+                    }
+                }
+                
+                children.push(split);
             }
         }
-        document.getElementById("bd").appendChild(div);
+
+        for (var i = 0; i < children.length; i++) {
+            document.getElementById("bd").appendChild(children[i]);
+        }
     } catch (e) {
         console.log(e.message);
+    }
+};
+
+function add_content(type, content, parent) {
+    if (type == "--h1") {
+        var h1 = document.createElement("h1");
+        
+        h1.innerHTML = content;
+        parent.appendChild(h1);
+    } 
+    else if (type == "--text") {
+        var p = document.createElement("p");
+        
+        p.innerHTML = content;
+        parent.appendChild(p);
     }
 };
 
